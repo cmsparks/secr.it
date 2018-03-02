80253b565b3b503e807925bf27c7417dc6648ac8
cmsparks <csparks1919@hotmail.com>
2018-03-02 16:37:07 -0600

    added in more hashing stuff

--- server/User.js
+++ server/User.js
@@ -21,33 +21,47 @@ const crypto = require('crypto');
 */
 
 class User {
-	constructor(isNew, data, socket, db) {
+	constructor(isNew, data, socket db) {
 		if(isNew === true){
 			this.name = data.name;
-			this.pwd = data.pwd;
+			
+			//get hashing
+			let rand = crypto.randomBytes(32);
+			let key;
+			crypto.pbkdf2(data.pwd, rand, 10000, 512, 'sha512', (err, derivedKey) => {
+			  if (err) throw err;
+			  key = derivedKey 
+			});
+
+			this.validation = {
+				'pwdhash': key,
+				'salt': rand,
+				'iter': 10000
+			};
+			this.currentChats = [];
+			//insert any opts/default opts
+			this.opts = {};
+
+			db.collection('users').findOne({'name': this.name}, null, function(err, result) {
+				console.log(obj + " " + err);
+			})
 		}
 		else {
-
+			db.collection('users').findOne({'name':data.name})
 		}
-		this.name = data.name;
-		this.validation = data.validation;
-		this.currentChats = data.currentChats;
-		this.opts = data.opts;
 	}
-	toDatabase() {
-		let data = {
-			'name': this.name,
-			'validation': this.validation,
-			'currentChats': this.currentChats,
-			'opts': this.opts
-		};
-		return data; 
-	}
-	validate(pwd) {
-		let isValid = false;
+	toDatabase(db) {
+		db.collection('users').updateOne(
+			{'name':this.name}, {
+        	$set: {
+            	'currentChats': this.currentChats,
+            	'opts': this.opts
+        	}
+    	});
 		
-
-		return isValid;
+	}
+	setOptions(opts) {
+		this.opts = opts;
 	}
 }
 
