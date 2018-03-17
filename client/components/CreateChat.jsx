27070a6f4ed40402f2eb3396cb801b50c7a4d679
cmsparks <csparks1919@hotmail.com>
2018-03-17 18:07:54 -0500

    styling changes

--- client/components/CreateChat.jsx
+++ client/components/CreateChat.jsx
@@ -6,17 +6,27 @@ class CreateChat extends React.Component {
 		this.state = {
 			chatname: '',
 			password: '',
-
+			users: '',
+			blacklist: '',
+			password: '',
 		};
 		this.createChat = this.createChat.bind(this);
 	}
 	render() {
 		return (
 			<form className="newChat" onSubmit={this.createChat}>
-				<input type="text" value={this.state.chatname} />
-				<input className="primaryBtn" type="submit" value="Create" />
-				<input className="secondaryBtn" type="submit" value="Other"/>
-				<input className="tertiaryBtn" type="submit" value="Cancel"/>
+				<div className="optsContainer">
+					<div className="name">Name: <input type="text" value={this.state.chatname} /></div>
+					<div className="isPublic"><input type="checkbox"/> Requires Invite </div>
+					<div className="hasPassword"><input type="checkbox"/> Has Password </div>
+					<div className="invites">Invite People: <input type="text" value={this.state.users} /></div>
+					<div className="blacklist">Blacklist: <input type="text" value={this.state.blacklist} /></div>
+					<div className="password">Password: <input type="text" value={this.state.password} /></div>
+				</div>
+				<div className="btnContainer">
+					<input className="primaryBtn" type="submit" value="Create" />
+					<input className="tertiaryBtn" type="submit" value="Cancel"/>
+				</div>
 			</form>
 		)
 	}
